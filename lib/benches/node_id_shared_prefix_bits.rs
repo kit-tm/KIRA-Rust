f2d132cf0e596e8a563729ca035b5ccdff3b5073
moblaa <11476187+MoBlaa@users.noreply.github.com>
2022-06-04 13:12:58 +0200

    custom preparation and time measurement for benchmarks

--- lib/benches/node_id_shared_prefix_bits.rs
+++ lib/benches/node_id_shared_prefix_bits.rs
@@ -1,43 +1,108 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use std::time::{Duration, Instant};
+
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
 use r2kad_lib::domain::{Id, NodeId};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("node_id_shared_prefix_bits");
     group.bench_function(BenchmarkId::from_parameter(1), |b| {
-        let first = NodeId::<1>::zero();
-        let second = NodeId::<1>::one();
-        b.iter(|| first.shared_prefix_bits(&second))
+        b.iter_custom(|iters| {
+            let mut elapsed = Duration::new(0, 0);
+            for _i in 0..iters {
+                let first = NodeId::<1>::random();
+                let second = NodeId::<1>::random();
+
+                let start = Instant::now();
+                black_box(first.shared_prefix_bits(&second));
+                elapsed += start.elapsed();
+            }
+            elapsed
+        })
     });
     group.bench_function(BenchmarkId::from_parameter(2), |b| {
-        let first = NodeId::<2>::zero();
-        let second = NodeId::<2>::one();
-        b.iter(|| first.shared_prefix_bits(&second))
+        b.iter_custom(|iters| {
+            let mut elapsed = Duration::new(0, 0);
+            for _i in 0..iters {
+                let first = NodeId::<2>::random();
+                let second = NodeId::<2>::random();
+
+                let start = Instant::now();
+                black_box(first.shared_prefix_bits(&second));
+                elapsed += start.elapsed();
+            }
+            elapsed
+        })
     });
     group.bench_function(BenchmarkId::from_parameter(4), |b| {
-        let first = NodeId::<4>::zero();
-        let second = NodeId::<4>::one();
-        b.iter(|| first.shared_prefix_bits(&second))
+        b.iter_custom(|iters| {
+            let mut elapsed = Duration::new(0, 0);
+            for _i in 0..iters {
+                let first = NodeId::<4>::random();
+                let second = NodeId::<4>::random();
+
+                let start = Instant::now();
+                black_box(first.shared_prefix_bits(&second));
+                elapsed += start.elapsed();
+            }
+            elapsed
+        })
     });
     group.bench_function(BenchmarkId::from_parameter(8), |b| {
-        let first = NodeId::<8>::zero();
-        let second = NodeId::<8>::one();
-        b.iter(|| first.shared_prefix_bits(&second))
+        b.iter_custom(|iters| {
+            let mut elapsed = Duration::new(0, 0);
+            for _i in 0..iters {
+                let first = NodeId::<8>::random();
+                let second = NodeId::<8>::random();
+
+                let start = Instant::now();
+                black_box(first.shared_prefix_bits(&second));
+                elapsed += start.elapsed();
+            }
+            elapsed
+        })
     });
     group.bench_function(BenchmarkId::from_parameter(16), |b| {
-        let first = NodeId::<16>::zero();
-        let second = NodeId::<16>::one();
-        b.iter(|| first.shared_prefix_bits(&second))
+        b.iter_custom(|iters| {
+            let mut elapsed = Duration::new(0, 0);
+            for _i in 0..iters {
+                let first = NodeId::<16>::random();
+                let second = NodeId::<16>::random();
+
+                let start = Instant::now();
+                black_box(first.shared_prefix_bits(&second));
+                elapsed += start.elapsed();
+            }
+            elapsed
+        })
     });
     group.bench_function(BenchmarkId::from_parameter(32), |b| {
-        let first = NodeId::<32>::zero();
-        let second = NodeId::<32>::one();
-        b.iter(|| first.shared_prefix_bits(&second))
+        b.iter_custom(|iters| {
+            let mut elapsed = Duration::new(0, 0);
+            for _i in 0..iters {
+                let first = NodeId::<32>::random();
+                let second = NodeId::<32>::random();
+
+                let start = Instant::now();
+                black_box(first.shared_prefix_bits(&second));
+                elapsed += start.elapsed();
+            }
+            elapsed
+        })
     });
     group.bench_function(BenchmarkId::from_parameter(64), |b| {
-        let first = NodeId::<64>::zero();
-        let second = NodeId::<64>::one();
-        b.iter(|| first.shared_prefix_bits(&second))
+        b.iter_custom(|iters| {
+            let mut elapsed = Duration::new(0, 0);
+            for _i in 0..iters {
+                let first = NodeId::<64>::random();
+                let second = NodeId::<64>::random();
+
+                let start = Instant::now();
+                black_box(first.shared_prefix_bits(&second));
+                elapsed += start.elapsed();
+            }
+            elapsed
+        })
     });
     group.finish();
 }
