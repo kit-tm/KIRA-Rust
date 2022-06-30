57438fb7f49c4f706895849a83cd8b877e6dd705
Moritz Hepp <moritz.hepp@student.kit.edu>
2022-06-30 15:17:32 +0200

    removed path validator

--- lib/src/domain/mod.rs
+++ lib/src/domain/mod.rs
@@ -6,7 +6,6 @@ pub use neighbor_table::*;
 pub use node_id::*;
 pub use path::*;
 pub use path_simplifier::*;
-pub use path_validator::*;
 pub use routing_table::flat_routing_table::*;
 pub use routing_table::*;
 
@@ -17,7 +16,6 @@ pub mod neighbor_table;
 pub mod node_id;
 pub mod path;
 pub mod path_simplifier;
-pub mod path_validator;
 pub mod routing_table;
 
 pub type Link = (NodeId, NodeId);
