a1cf7de9250efee3363211a71197707707b21978
moblaa <11476187+MoBlaa@users.noreply.github.com>
2022-09-06 17:01:32 +0200

    changed contact update to contain old and new value

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -66,7 +66,7 @@ fn main() {
         let contact_event = match event {
             RoutingTableEvent::NewContact(contact) => ContactEvent::New(contact),
             RoutingTableEvent::RemovedContact(contact) => ContactEvent::Removed(contact),
-            RoutingTableEvent::UpdatedContact(contact) => ContactEvent::Updated(contact),
+            RoutingTableEvent::UpdatedContact { new, old } => ContactEvent::Updated { new, old },
             _ => return,
         };
         if let Err(e) = observer_broadcaster.send(UseCaseEvent::Contact(contact_event)) {
