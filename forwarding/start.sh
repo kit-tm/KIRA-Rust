4e8a8902d573aac7662bffb6e27f42fe4190826f
Jakob Müllner <uifnz@student.kit.edu>
2023-05-05 12:58:25 +0200

    simplify setup

--- forwarding/start.sh
+++ forwarding/start.sh
@@ -1,26 +1,16 @@
 #!/bin/sh
 
-# Usage start.sh <nodeID> <command>
-# where command is an arbitrary shell script
+# Usage start.sh <nodeID>
+
 nodeID=$1
 
 echo "NodeID is $nodeID."
 echo "Setting up forwarding..."
 
 # Setup ip6gre interface for encapsulation/decapsulation
-ip link add name kira type ip6gre local beef::a remote beef::b
+ip link add name kira type ip6gre external
 # ip addr add $nodeID/16 dev kira
 ip link set kira up
 
 # load nftables rules
 nft -f nftables.conf
-
-# setup routing policy
-ip -6 rule add from all fwmark 0xff00 lookup 65280
-ip -6 route add default dev kira table 65280
-
-# setup nodeID
-#ip addr add $nodeID/16 dev eth0
-
-# setup GRE interface (why is this necessary?)
-#ip addr add beef::a dev eth0
