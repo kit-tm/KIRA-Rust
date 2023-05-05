572a0d6e398de0671b6b982941f2ba51901c6d53
Jakob Müllner <uifnz@student.kit.edu>
2023-05-05 12:57:29 +0200

    add simple forwarding test

---
+++ forwarding/tests/forwarding.py
@@ -0,0 +1,39 @@
+from mininet.cli import CLI
+from mininet.net import Containernet
+from mininet.node import Controller
+from mininet.log import info, setLogLevel
+from time import sleep
+
+setLogLevel('info')
+
+SYSCTL = {'net.ipv6.conf.all.disable_ipv6': 0}
+
+net = Containernet(controller=Controller)
+d1 = net.addDocker(name="d1", sysctls=SYSCTL, cap_add=['net_admin'], dimage="kira")
+d2 = net.addDocker(name="d2", sysctls=SYSCTL, cap_add=['net_admin'], dimage="kira")
+d3 = net.addDocker(name="d3", sysctls=SYSCTL, cap_add=['net_admin'], dimage="kira")
+
+net.addLink(d1, d2)
+net.addLink(d2, d3)
+
+net.start()
+
+info(d1.cmd("./start.sh fc00::1"))
+info(d1.cmd("./nodetopath.sh fc00::3 fcaa::2"))
+info(d1.cmd("ip a add fc00::1/16 dev d1-eth0"))
+info(d1.cmd("ip a add fcaa::1/16 dev d1-eth0"))
+
+info(d2.cmd("./start.sh fc00::2"))
+info(d2.cmd("./pathtopath.sh fcaa::2 fcaa::3"))
+info(d2.cmd("./pathtopath.sh fcaa::4 fcaa::1"))
+info(d2.cmd("ip a add fcaa::2/16 dev d2-eth0"))
+info(d2.cmd("ip a add fcaa::4/16 dev d2-eth1"))
+
+info(d3.cmd("./start.sh fc00::3"))
+info(d3.cmd("./nodetopath.sh fc00::1 fcaa::4"))
+info(d3.cmd("ip a add fc00::3/16 dev d3-eth0"))
+info(d3.cmd("ip a add fcaa::3/16 dev d3-eth0"))
+
+CLI(net)
+
+net.stop()
