264c9e71e4edd67ccbee5a551c12bf47f5e64950
Jakob Müllner <uifnz@student.kit.edu>
2023-06-23 14:33:10 +0200

    add masquerade for outgoing encapsulated packets

--- forwarding/start.sh
+++ forwarding/start.sh
@@ -9,8 +9,9 @@ echo "Setting up forwarding..."
 
 # Setup ip6gre interface for encapsulation/decapsulation
 ip link add name kira type ip6gre external
-# ip addr add $nodeID/16 dev kira
 ip link set kira up
 
 # load nftables rules
+echo "define localNodeIP = $nodeID\n" | cat - nftables.conf > temp
+mv temp nftables.conf
 nft -f nftables.conf
