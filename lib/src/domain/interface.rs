e9e70c8c1cc49dd3c685fac499aadd4d215c4645
Jakob Müllner <uifnz@student.kit.edu>
2023-07-28 13:35:39 +0000

    add index to network interface

--- lib/src/domain/interface.rs
+++ lib/src/domain/interface.rs
@@ -3,21 +3,24 @@ use std::fmt::{Display, Formatter};
 #[cfg(feature = "pnet")]
 pub use pnet_conversion::*;
 
-/// Represents network interface (as in 'hardware device') by name.
+/// Represents network interface (as in 'hardware device') by interface index.
 #[derive(Debug, PartialEq, Eq, Clone, Hash)]
 pub struct NetworkInterface {
-    pub name: String,
+    pub index: u32,
+    pub name: String
 }
 
 impl NetworkInterface {
-    pub fn new<S: Into<String>>(name: S) -> Self {
-        Self { name: name.into() }
+    pub fn new(index: u32) -> Self {
+        let interfaces = pnet::datalink::interfaces();
+        let interface = interfaces.iter().find(|i| i.index == index).expect("Unable to find interface");
+        Self {index, name: interface.name.clone()}
     }
 }
 
 impl Display for NetworkInterface {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "NetworkInterface [\"{}\"]", self.name)
+        write!(f, "NetworkInterface index [\"{}\"]", self.index)
     }
 }
 
@@ -29,7 +32,7 @@ mod pnet_conversion {
 
     impl<'a> From<&'a datalink::NetworkInterface> for NetworkInterface {
         fn from(iface: &'a datalink::NetworkInterface) -> Self {
-            NetworkInterface::new(iface.name.clone())
+            NetworkInterface::new(iface.index)
         }
     }
 }
