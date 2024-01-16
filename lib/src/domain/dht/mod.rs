8df694fe247bf8de7e7c899c0971c0201e34db9f
Julius Rüberg <julius.rueberg@protonmail.ch>
2024-01-16 22:28:03 +0100

    Remove Serialize implementation of TimedValue

--- lib/src/domain/dht/mod.rs
+++ lib/src/domain/dht/mod.rs
@@ -20,29 +20,10 @@ mod expiring;
 ///
 /// ## [Eq] and [Hash]
 /// It is important to note that this struct only derives [Eq] and [Hash] from the `value`.
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone)]
 pub struct TimedValue<V> {
     pub value: V,
-    #[serde(with = "approx_instant")]
     pub timestamp: Instant,
-}
-
-mod approx_instant {
-    // https://github.com/serde-rs/serde/issues/1375#issuecomment-419688068
-    use std::time::{Instant, SystemTime};
-    use chrono::{Local, DateTime};
-    use serde::{Serialize, Serializer};
-
-    pub fn serialize<S>(instant: &Instant, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
-    {
-        let system_now = SystemTime::now();
-        let instant_now = Instant::now();
-        let approx = system_now - (instant_now - *instant);
-        let now: DateTime<Local> = approx.into();
-        now.to_rfc3339().serialize(serializer)
-    }
 }
 
 impl<V> TimedValue<V> {
