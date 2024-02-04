e58d5a82bc49ae867b01056aef7168a79bce06fd
Julius Rüberg <julius.rueberg@protonmail.ch>
2024-02-04 16:32:47 +0000

    WIP Fix sending UpdateRouteReq

--- lib/src/use_cases/handle_contact_update.rs
+++ lib/src/use_cases/handle_contact_update.rs
@@ -7,6 +7,7 @@ use crate::domain::{Contact, ContactState, NotVia, PNTable, RoutingTable};
 use crate::messaging::source_route::SourceRoute;
 use crate::messaging::{ProtocolMessageSender, RouteUpdate, UpdateRouteReq};
 use crate::use_cases::{ContactEvent, EventHandler, MessageSentFailed, UseCaseEvent};
+use crate::use_cases::UseCaseEvent::Contact;
 
 /// Configuration for contact update handling.
 #[derive(Debug)]
@@ -122,11 +123,16 @@ where
     ) -> Result<Self::Value, Self::Error> {
         match event {
             UseCaseEvent::Contact(ContactEvent::Removed(contact)) => {
-                let mut updates = HashMap::new();
-                updates.insert(contact.clone(), RouteUpdate::Removed);
+                // only send update if physical neighbor got removed
+                // this doesn't happen in reality since we use the UnlimitedPNRoutingTable
+                if contact.is_pn() {
+                    let mut updates = HashMap::new();
+                    updates.insert(contact.clone(), RouteUpdate::Removed);
 
-                // fixme dont send update unless physical neighbor src/routing/r2kademlia/R2Kademlia.cc:223
-                self.send_update(context, updates)?;
+                    log::trace!(target: "handle_contact_update", "Sending update concerning removal of physical neighbor {}", contact.id());
+                    self.send_update(context, updates)?;
+                }
+
                 context.not_via_mut().retain(|not_via| match not_via {
                     NotVia::Link(link) => {
                         link.first() != contact.id() && link.second() != contact.id()
@@ -143,12 +149,20 @@ where
                 let mut updates = HashMap::new();
                 updates.insert(new.clone(), RouteUpdate::Updated);
 
-                // fixme only send update on improvement src/routing/r2kademlia/R2KademliaPolicyHandlers.cc:303
+                // this should be fine, since we only update contacts if interesting anyways
                 self.send_update(context, updates)?;
                 if old.state() == &ContactState::Valid && new.state() != &ContactState::Valid {
                     // Add to not-via data if path gets invalid (maybe done already)
                     self.invalidate_all_affected_contacts(context, &new);
                 }
+            }
+            UseCaseEvent::Contact(ContactEvent::New(new)) => {
+                // always send an update if a new contact was found
+                let mut updates = HashMap::new();
+                updates.insert(new.clone(), RouteUpdate::Updated);
+
+                log::trace!(target: "handle_contact_update", "New contact {} found. Sending update.", new.id());
+                self.send_update(context, updates)?;
             }
             _ => {}
         }
