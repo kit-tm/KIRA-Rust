cf656b4cdf3931fb2b62b76658d5d630ae2ee4b6
Jakob Müllner <uifnz@student.kit.edu>
2024-02-29 10:54:48 +0000

    implement updating forwarding rules

--- lib/src/forwarding/platform/linux.rs
+++ lib/src/forwarding/platform/linux.rs
@@ -50,6 +50,12 @@ pub fn add_forwarding_rule(from: Ipv6Addr, to: Ipv6Addr) -> Result<(), String> {
     }
 }
 
+pub fn update_forwarding_rule(from: Ipv6Addr, to: Ipv6Addr) -> Result<(), String> {
+    // TODO make this (and the other nft operations) atomic with: printf <config> | nft -f -
+    delete_forwarding_rule(from)?;
+    add_forwarding_rule(from, to)
+}
+
 pub fn delete_forwarding_rule(from: Ipv6Addr) -> Result<(), String> {
     let from_addr = from.to_string();
     let output = Command::new("nft")
