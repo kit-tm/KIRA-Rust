db831d3d528f350049217c9e07d6081be58c18dc
Julius Rüberg <julius.rueberg@protonmail.ch>
2024-03-01 19:39:18 +0100

    Don't disable eth0 but exclude KIRA@NONE interface since sending to it causes trouble

--- daemon/docker/r2kad-daemon.sh
+++ daemon/docker/r2kad-daemon.sh
@@ -3,6 +3,5 @@
 # Setup ip6gre interface for encapsulation/decapsulation
 ip link add name kira type ip6gre external
 ip link set kira up
-ip link set eth0 down
 
-./r2kad-daemon
+./r2kad-daemon --excluded-interfaces=4
