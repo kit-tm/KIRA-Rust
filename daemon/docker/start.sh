d04b014cefdfdcf2bbf85d730f488e1765dc813f
Julius Rüberg <julius.rueberg@protonmail.ch>
2024-03-27 19:40:58 +0000

    Remove unneeded operations from entrypoint scripts

--- daemon/docker/start.sh
+++ daemon/docker/start.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-
-# Setup ip6gre interface for encapsulation/decapsulation
-ip link add name kira type ip6gre external
-ip link set kira up
-ip link set eth0 down
-
 until [ -f /r2kad-daemon/start ]
 do
      sleep 5
