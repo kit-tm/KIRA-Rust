823cc220c8ea1f6afdae1d65bf91a57ab1d64e95
Julius Rüberg <julius.rueberg@protonmail.ch>
2024-04-10 23:36:20 +0000

    Add script to test connectivity via pings

---
+++ daemon/demo/test_all.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+for i in {0..9}
+do
+    for o in {0..9}
+    do
+        docker exec ${PREFIX}r2kad-n$i /usr/bin/ping -c 3 -i 0.25 -W 1 -q fc00::$o 2>&1 > /dev/null \
+            || echo "$i -> $o"
+    done
+done
