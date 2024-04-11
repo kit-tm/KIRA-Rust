95f54ca8c55e3775ea9d47f06762c5780ff40446
Julius Rüberg <julius.rueberg@protonmail.ch>
2024-04-11 13:53:38 +0000

    Add option to specify the naming scheme of the container

--- daemon/demo/test_all.sh
+++ daemon/demo/test_all.sh
@@ -4,7 +4,7 @@ for i in {0..9}
 do
     for o in {0..9}
     do
-        docker exec ${PREFIX}r2kad-n$i /usr/bin/ping -c 3 -i 0.25 -W 1 -q fc00::$o 2>&1 > /dev/null \
+        docker exec ${PREFIX}${NAME:=r2kad-n}$i /usr/bin/ping -c 3 -i 0.25 -W 1 -q fc00::$o 2>&1 > /dev/null \
             || echo "$i -> $o"
     done
 done
