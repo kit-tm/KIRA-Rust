75ceff933a62808843a260b1e30fee57461c97f3
Julius Rüberg <julius.rueberg@student.kit.edu>
2025-07-14 20:36:55 +0200

    Coherent formating

--- tests/show-graph.py
+++ tests/show-graph.py
@@ -8,12 +8,12 @@ def main(args):
     G = nx.readwrite.read_gml(args.test_gml)
 
     pos = nx.kamada_kawai_layout(G)
-    nx.draw_networkx(G, pos=pos, font_color='w')
+    nx.draw_networkx(G, pos=pos, font_color="w")
     plt.savefig(sys.stdout.buffer, transparent=True, dpi=200)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Show Grpah of gml')
-    parser.add_argument('test_gml', type=str, help="The gml file")
+    parser = argparse.ArgumentParser(description="Show Grpah of gml")
+    parser.add_argument("test_gml", type=str, help="The gml file")
     args = parser.parse_args()
     main(args)
