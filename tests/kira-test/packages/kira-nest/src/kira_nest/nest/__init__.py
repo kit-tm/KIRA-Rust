b6b0905742d0ffa9b92fb1850d95ec1283386be8
Julius Rüberg <julius.rueberg@student.kit.edu>
2026-04-01 04:26:48 +0200

    Use unshare(2) to allow unprivileged run of NeST-Test Shell.

--- tests/kira-test/packages/kira-nest/src/kira_nest/nest/__init__.py
+++
@@ -1,5 +0,0 @@
-from kira_nest.nest.imager import KIRAImager
-from kira_nest.nest.link import KIRALink
-from kira_nest.nest.node import KIRANode
-from kira_nest.nest.shell import DebugShell
-from kira_nest.nest.test import KIRATest
