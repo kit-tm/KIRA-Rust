kit-tm/KIRA-Rust

commit da2875c0236408945a2d30f7dce0b4cdf90436dd

fixed clippy warnings from 1.97

kira-r2kad/src/use_cases/failure_handling.rs
@@ -151,34 +151,32 @@ where
         let mut next_via_contact_ids: Vec<NodeId> = Vec::new();
         let mut processed_via_contacts: usize = 0;
         // get contact for rediscovery state
-        if let Some(contact) = context.routing_table().contact(contact_id) {
-            // if contact state is still rediscovering get next via contacts to try
-            if let ContactState::Rediscovering(rds) = contact.state() {
-                // contact is still in rediscovering
-                // get next via contacts from list that exists and is valid (note that this is then removed from the list in rds)
-                let via_contact_list = rds.get_via_contact_list();
-                let via_contact_list_iter = via_contact_list.iter().enumerate();
-                for (idx, next_via_contact_id) in via_contact_list_iter {
-                    processed_via_contacts = idx + 1;
-                    if context
-                        .routing_table()
-                        .contains_with(next_via_contact_id, |c| c.is_valid())
-                    {
-                        next_via_contact_ids.push(*next_via_contact_id);
-                    }
-                    if next_via_contact_ids.len() == no_of_via_contacts {
-                        break;
-                    }
+        // if contact does not exist anymore then terminate Rediscovery
+        let rt = context.routing_table();
+        let contact = rt.contact(contact_id)?;
+        // if contact state is still rediscovering get next via contacts to try
+        if let ContactState::Rediscovering(rds) = contact.state() {
+            // contact is still in rediscovering
+            // get next via contacts from list that exists and is valid (note that this is then removed from the list in rds)
+            let via_contact_list = rds.get_via_contact_list();
+            let via_contact_list_iter = via_contact_list.iter().enumerate();
+            for (idx, next_via_contact_id) in via_contact_list_iter {
+                processed_via_contacts = idx + 1;
+                if context
+                    .routing_table()
+                    .contains_with(next_via_contact_id, |c| c.is_valid())
+                {
+                    next_via_contact_ids.push(*next_via_contact_id);
                 }
-            } else {
-                // contact exists but is not in rediscovery state anymore, so simply ignore this timeout or message
-                return None;
+                if next_via_contact_ids.len() == no_of_via_contacts {
+                    break;
+                }
             }
-        } else {
-            // if contact does not exist anymore then terminate Rediscovery
-            return None;
-        };
-
+        }
+        else {
+            // contact exists but is not in rediscovery state anymore, so simply ignore this timeout or message
+            return None
+        }
         // now we need to delete the processed entries from the via contact list in a separate pass
         // due to mutable borrow
         if processed_via_contacts > 0

kira-forwarding/src/tables/native_tables.rs
@@ -171,7 +171,7 @@ where
                 // The via route to this contact is already configured.
                 // To avoid unnecessary reconfiguration we don't configure the via route again.
                 if prefix_length == 128 {
-                    log::trace!(target: "native_fwd_table", "Trying to replace route to {:?} via {:?}", &out_path_id, &next_hop);
+                    log::trace!(target: "native_fwd_table", "Trying to replace route to {:?} via {:?}", out_path_id, next_hop);
                     let next_hop = self
                         .underlay_information_provider
                         .get_information(next_hop)
@@ -180,7 +180,7 @@ where
                     self.netlink
                         .replace_via_route(out_path_id, &next_hop)
                         .await
-                        .unwrap_or_else(|err|{log::error!(target: "native_fwd_table", "Failed to replace route to {:?} via {:?} error={}", &out_path_id, &next_hop, err)});
+                        .unwrap_or_else(|err|{log::error!(target: "native_fwd_table", "Failed to replace route to {:?} via {:?} error={}", out_path_id, next_hop, err)});
                 }
             }
         }

kira-lib/src/io/receiver/udp_tokio.rs
@@ -162,7 +162,7 @@ impl AsyncProtocolMessageReceiver for UdpReceiver {
                 log::trace!(target: "message_receiver", "Ignoring message from us");
                 continue;
             }
-            log::trace!(target: "message_receiver", "Received {:?} from {}", &message, received_from);
+            log::trace!(target: "message_receiver", "Received {:?} from {}", message, received_from);
 
             let neighbor = UnderlayNeighbor::new(*received_from.ip(), interface_id);
             let ulnid = match self.underlay_handle.register_neighbor(neighbor).await {

kira-r2kad/src/use_cases/inject_messages.rs
@@ -158,7 +158,7 @@ where
                 }
                 let source_route = closest_route.unwrap();
 
-                log::trace!(target: "inject_messages", "Sending FindNodeReq from {} with target {}", source_route.source(), &data.target);
+                log::trace!(target: "inject_messages", "Sending FindNodeReq from {} with target {}", source_route.source(), data.target);
 
                 let nonce = nonce.unwrap_or_else(|| {
                     // generate distinct nonce

commit 1721e7ee8c688a91932e49442e7bbe7143946984

fixed panicking in failure_handling introduced by commit da2875c0236408945a2d30f7dce0b4cdf90436dd send_message will not broadcast message anymore in case the next hop is unknown

kira-r2kad/src/runtime/mod.rs
@@ -57,30 +57,29 @@ pub trait UseCaseRuntime {
 
     /// Convenient method to send a [ProtocolMessage].
     ///
-    /// If the next hop is not in the `un_table` (underlay neighbor table)
-    /// or the even is not source-routed like [ULNHello messages](crate::messaging::ProtocolMessage::ULNHello)
-    /// the event is delivered by broadcasting to all interfaces.
+    /// If the next hop is not in the `uln_table` (underlay neighbor table)
+    /// or the even is source-routed like [ULNHello messages](crate::messaging::ProtocolMessage::ULNHello)
+    /// a warning is logged
     fn send_message<P: Into<ProtocolMessage>>(
         &self,
         protocol_message: P,
         ulntable: &impl Deref<Target = HashMap<NodeId, UnderlayNeighborId>>,
     ) {
         let protocol_message: ProtocolMessage = protocol_message.into();
-        let underlay_dest = protocol_message
-            .current_hop()
-            .and_then(|next_hop| {
-                let uln_dest = ulntable.get(next_hop);
-                if uln_dest.is_none() {
-                    tracing::warn!(
-                        %next_hop,
-                        reason = "dest_next_hop_unknown",
-                        "Fallback to broadcast message delivery"
-                    );
-                }
-                uln_dest
-            })
-            .copied()
-            .into();
+        let underlay_dest = if let Some(next_hop) = protocol_message.current_hop() {
+            let Some(uln_dest) = ulntable.get(next_hop) else {
+                tracing::warn!(
+                    %next_hop,
+                    reason = "uln_dest of next hop unknown",
+                    ?protocol_message,
+                    "Dropping message"
+                );
+                return;
+            };
+            (*uln_dest).into()
+        } else {
+            UnderlayNeighborDestination::Broadcast
+        };
 
         self.send_message_via(protocol_message, underlay_dest);
     }

kira-r2kad/src/use_cases/failure_handling.rs
@@ -152,10 +152,10 @@ where
         let mut processed_via_contacts: usize = 0;
         // get contact for rediscovery state
         // if contact does not exist anymore then terminate Rediscovery
-        let rt = context.routing_table();
-        let contact = rt.contact(contact_id)?;
         // if contact state is still rediscovering get next via contacts to try
-        if let ContactState::Rediscovering(rds) = contact.state() {
+        if let ContactState::Rediscovering(rds) =
+            context.routing_table().contact(contact_id)?.state()
+        {
             // contact is still in rediscovering
             // get next via contacts from list that exists and is valid (note that this is then removed from the list in rds)
             let via_contact_list = rds.get_via_contact_list();